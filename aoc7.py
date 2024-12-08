69b65afab091c5b1792a133fdfbb2fb758cbb9a6
sarah <s.onrust@student.utwente.nl>
2024-12-08 11:28:56 +0100

    Day 7

--- aoc7.py
+++ aoc7.py
@@ -20,13 +20,16 @@ for n in range(len(numbers)):
                 option = options[o]
                 new_sum = option + num
                 new_mul = option * num
+                # commend out this next line for part 1
                 new_con = str(option) + str(num)
                 options[o] = new_sum
                 options.append(new_mul)
+                # commend out this next line for part 1
                 options.append(int(new_con))
     if int(result[n]) in options:
         count = count + int(result[n])
 
+#print("Deel 1: " + str(count))
 print("Deel 2: " + str(count))
 
 
